a59e955691d23310e85c94e7c534438ca615f518
Emeralda0144 <kwunder014@gmail.com>
2022-04-08 21:22:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,10 @@ int main(void)
 
 	while (e < 58)
 	{
-		i = 48;											while (i < 58)
-		{												if (e != i && e < i)
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
 			{
 				putchar(e);
 				putchar(i);
