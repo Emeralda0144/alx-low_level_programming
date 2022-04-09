a597cf902e401ab8ead7ba76ffe7fb97848e4382
Emeralda0144 <kwunder014@gmail.com>
2022-04-08 18:20:12 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		putchar(ch);
-	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
 		putchar(ch);
-	}
 	putchar('\n');
 	return (0);
 }
