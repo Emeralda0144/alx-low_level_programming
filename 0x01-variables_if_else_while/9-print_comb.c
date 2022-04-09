f621e9a084cdc0f9ca71b13e0622544d5b1a40f2
Emeralda0144 <kwunder014@gmail.com>
2022-04-08 19:59:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
@@ -10,10 +10,12 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		putchar(n);									if (n != 57)									{
-			putchar(',');									putchar(' ');
-		}
-	}
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}									}
 	putchar('\n');
 	return (0);
 }
